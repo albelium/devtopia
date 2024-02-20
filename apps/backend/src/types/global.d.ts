d321fee552571fb74f43c4056bf6a861a71ff51d
shirai <ryhshr3@gmail.com>
2024-02-20 23:44:27 +0900

    chore: 環境変数追加

--- apps/backend/src/types/global.d.ts
+++ apps/backend/src/types/global.d.ts
@@ -2,8 +2,17 @@
 
 declare namespace NodeJS {
   interface ProcessEnv {
-    readonly NODE_ENV: 'development' | 'testing' | 'production'
+    readonly NODE_ENV: 'development' | 'staging' | 'production'
     readonly PORT: number
     readonly GRAPHQL_PATH: string
+    readonly DATABASE_URL: string
+
+    // Auth0
+    readonly AUTH0_DOMAIN: string
+    readonly AUTH0_CLIENT_ID: string
+    readonly AUTH0_CLIENT_SECRET: string
+    readonly AUTH0_JWKS_URL: string
+    readonly AUTH0_AUDIENCE: string
+    readonly AUTH0_ISSUER: string
   }
 }
